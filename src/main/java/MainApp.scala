8792fd37e35ab306d2655cd59c2d909f8377267e
SANABRIA GARCIA,ISAAC <isaac.sanabria@gft.com>
2021-07-20 23:35:22 +0200

    Added json schema support

--- src/main/java/MainApp.scala
+++ src/main/java/MainApp.scala
@@ -1,47 +1,31 @@
+import generator.DataGenerator.{createJsonSchemaDF, createSimpleSchemaDF}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
 
 object MainApp extends App {
-  //TODO: 5) Meterlo en git (con gitignore)
   //TODO: 1) Añadir schemas json
   //TODO: 2) Generar datos como hacía Dani
   //TODO: 3) Meterle algunas operaciónes básicas
   //TODO: 4) Encapsular en Docker
-
-
 
   val conf = new SparkConf().setMaster("local").setAppName("MyBasicSpark")
 
   //val sc = new SparkContext(conf)
   //val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-  val spark = SparkSession
+  protected implicit lazy val spark = SparkSession
     .builder()
     .appName("Spark SQL basic example")
     .config(conf)
     .getOrCreate()
 
+  val df = createSimpleSchemaDF
+  df.printSchema()
+  df.show()
 
-    val simpleData = Seq(Row("James","","Smith","36636","M",3000),
-      Row("Michael","Rose","","40288","M",4000),
-      Row("Robert","","Williams","42114","M",4000),
-      Row("Maria","Anne","Jones","39192","F",4000),
-      Row("Jen","Mary","Brown","","F",-1)
-    )
-
-    val simpleSchema = StructType(Array(
-      StructField("firstname",StringType,true),
-      StructField("middlename",StringType,true),
-      StructField("lastname",StringType,true),
-      StructField("id", StringType, true),
-      StructField("gender", StringType, true),
-      StructField("salary", IntegerType, true)
-    ))
-
-    val df = spark.createDataFrame(spark.sparkContext.parallelize(simpleData),simpleSchema)
-
-    df.printSchema()
-    df.show()
+  //val jsonDf = createJsonSchemaDF
+  //jsonDf.printSchema()
+  //jsonDf.show()
 
 }
---
+++ src/main/java/generator/DataGenerator.scala
@@ -0,0 +1,51 @@
+package generator
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DataType, IntegerType, StringType, StructField, StructType}
+
+import scala.io.Source
+
+
+object DataGenerator {
+
+  def createSimpleSchemaDF(implicit spark: SparkSession) = {
+
+    val simpleData = Seq(Row("James","","Smith","36636","M",3000),
+      Row("Michael","Rose","","40288","M",4000),
+      Row("Robert","","Williams","42114","M",4000),
+      Row("Maria","Anne","Jones","39192","F",4000),
+      Row("Jen","Mary","Brown","","F",-1)
+    )
+
+    val simpleSchema = StructType(Array(
+      StructField("firstname",StringType,true),
+      StructField("middlename",StringType,true),
+      StructField("lastname",StringType,true),
+      StructField("id", StringType, true),
+      StructField("gender", StringType, true),
+      StructField("salary", IntegerType, true)
+    ))
+
+    spark.createDataFrame(spark.sparkContext.parallelize(simpleData), simpleSchema)
+
+  }
+
+  def createJsonSchemaDF(implicit spark: SparkSession) = {
+
+    val arrayStructureData = Seq(
+      Row(Row("James","","Smith"),"Cricket","female",10000),
+      Row(Row("Michael","Rose",""),"Tennis","male",20000),
+      Row(Row("Robert","","Williams"),"Cooking","male",30000),
+      Row(Row("Maria","Anne","Jones"),null,"female",20000),
+      Row(Row("Jen","Mary","Brown"),"Blogging","female",10000)
+    )
+
+    val url = ClassLoader.getSystemResource("schemas/person.json")
+    val schemaSource = Source.fromFile(url.getFile).getLines.mkString
+    val schemaFromJson = DataType.fromJson(schemaSource).asInstanceOf[StructType]
+
+    spark.createDataFrame(spark.sparkContext.parallelize(arrayStructureData),schemaFromJson)
+
+  }
+
+}
